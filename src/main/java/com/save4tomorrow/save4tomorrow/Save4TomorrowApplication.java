0a2db071a4f03595901dea5519bdf14ffff8e069
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-12-15 10:35:40 -0600

    Created Enviorment for project

---
+++ src/main/java/com/save4tomorrow/save4tomorrow/Save4TomorrowApplication.java
@@ -0,0 +1,13 @@
+package com.save4tomorrow.save4tomorrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Save4TomorrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Save4TomorrowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/save4tomorrow/save4tomorrow/Save4TomorrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.save4tomorrow.save4tomorrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Save4TomorrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
