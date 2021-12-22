b039aa0daad0b77c0065aeff6d02f7749ef2e9c2
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-12-22 17:53:24 -0600

    Adding in the weather API

--- src/main/resources/static/weather.js
+++ src/main/resources/static/weather.js
@@ -1,1 +1,12 @@
-const WEATHER_API_KEY = "af490826bd781f8c6df4775c5164833f\n";
+// const WEATHER_API_KEY = "af490826bd781f8c6df4775c5164833f\n";
+console.log("Hello World")
+fetch("https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=af490826bd781f8c6df4775c5164833f")
+    .then((result) => {
+        return result.json()
+    }).then((data) => {
+    console.log(data)
+    let currentWeather = document.querySelector("#currentWeather")
+    currentWeather.innerHTML += `
+    <h1>°</h1>
+    `
+})
