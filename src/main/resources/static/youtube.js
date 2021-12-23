e202442ab8ae96e63f6e59915a7493a7b9549d09
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-12-22 21:28:41 -0600

    Updated resources page

--- src/main/resources/static/youtube.js
+++ src/main/resources/static/youtube.js
@@ -1,10 +1,11 @@
 // const YOUTUBE_API_KEY = "AIzaSyAvK0vlvFQ_o11jqZ45-iFVaw_mCTY9H6Y";
+// AIzaSyC3Q0K6kAG0qce-oA4DnCMtrth7fI1iuuc
 console.log("hello world")
 let playlistID
 let nextPageToken =""
 
 // function getVideos() {
-    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCaL8K8g8y8K4_CvNuN0BDtg&maxResults=10&order=date&key=AIzaSyAvK0vlvFQ_o11jqZ45-iFVaw_mCTY9H6Y&pageToken=" +nextPageToken)
+    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCaL8K8g8y8K4_CvNuN0BDtg&maxResults=10&order=date&key=AIzaSyC3Q0K6kAG0qce-oA4DnCMtrth7fI1iuuc&pageToken=" +nextPageToken)
         .then((result) => {
             return result.json()
         }).then((data) => {
@@ -16,7 +17,7 @@ let nextPageToken =""
             console.log(video.snippet.title)
             videoContainer.innerHTML += `
         <div id="youtubeContainer">
-        <img id="youtube" src="${video.snippet.thumbnails.high.url}">
+        <img id="youtube" src="${video.snippet.thumbnails.medium.url}">
         <h3>${video.snippet.title}</h3>
         </div>
         `
